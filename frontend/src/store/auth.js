59451cd039cf807ca8c45c2253701dbaa046c32d
Your Name <your.email@example.com>
2024-03-14 17:24:26 +0530

    first commit

---
+++ frontend/src/store/auth.js
@@ -0,0 +1,33 @@
+import { createContext, useContext, useState } from "react";
+
+export const AuthContext = createContext();
+
+export const AuthProvider = ({children})=>{
+    const[token,setToken] = useState(localStorage.getItem("token"));
+
+
+    const storetokeninLS =(serverToken)=>{
+        return localStorage.setItem('token',serverToken);
+
+    }
+    let isLoggedIn = !!token;
+    console.log('isloggedin',isLoggedIn);
+    //logout
+    const LogoutUser =()=>{
+        setToken("");
+        return localStorage.removeItem("token")
+
+    }
+    return (<AuthContext.Provider value ={{isLoggedIn,storetokeninLS,LogoutUser}}>
+        {children}
+
+    </AuthContext.Provider>
+    );
+};
+
+export const useAuth=()=>{
+    const authContextValue = useContext(AuthContext);
+    return authContextValue;
+
+}
+
